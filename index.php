97e8df467926c069fb4a6be6ce3f6600bb26e17b
ALEXWALLACEARAUJODES <ALEXWALLACEARAUJODES@DESKTOP-QD13R35>
2018-02-18 22:30:05 -0300

    início projeto campeonatos gamer

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Campeonatos Gamer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/estilo.css">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.js"></script>
+
+    </head>
+    <body>
+        <div id="geral">
+            <nav class="navbar-fixed-top navbar-inverse navbar-transparente">
+                <div class="container">
+                    <!-- Header -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed"
+                                data-toggle="collapse" data-target="#barra-navegacao">
+
+                            <span class="sr-only">Alternar navegação</span>
+                            <span class="icon-bar"></span> <!-- esses 3 spans faz aquele simbolo de quadrado co tres tiras dentro -->
+                            <span class="icon-bar"></span> <!-- esses 3 spans faz aquele simbolo de quadrado co tres tiras dentro -->
+                            <span class="icon-bar"></span> <!-- esses 3 spans faz aquele simbolo de quadrado co tres tiras dentro -->
+                        </button>
+
+                        <a href="index.php" class="navbar-brand">
+                            <span class="logo">Campeonatos Gamer</span>
+                        </a>
+
+                    </div> <!-- fim do Header -->
+                    <!-- navbar --> 
+                    <div class="collapse navbar-collapse" id="barra-navegacao">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#">Campeonatos</a></li>
+                            <li><a href="#">Menu</a></li>
+                            <li><a href="#">Entrar</a></li>
+                        </ul>
+                    </div>  <!-- fim do navbar -->  
+
+                </div> <!-- fim do container -->
+
+            </nav> <!-- fim do navbar --> 
+
+            <div class="corpo">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>CAMPEONATOS GAMES</h1>
+                    </div>
+                </div>
+
+
+                <div class="menu">
+                    <div class="row">
+                        <div id="corpo-direito" class="col-md-4">
+                         
+                        </div>
+                        <div id="corpo-centro" class="col-md-4">
+                            <label for="cadastro"><h2>Cadastre-se</h2></label><br />
+                            <button type="submit" class="btn btn-primary"><h4>Cadastrar</h4></button><br /><br /><br />
+                            <label for="entrar"><h2>Entrar</h2></label><br />
+                            <button type="submit" class="btn btn-primary"><h4>Entrar</h4></button>
+                        </div>
+                        <div id="corpo-esquerdo" class="col-md-4">
+                        
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div> <!-- fim da div corpo --> 
+            
+            <div class="rodape">
+                <div class="row">
+                    <div class="col-md-12">
+                        
+                    </div>
+                </div>
+                
+            </div>
+            
+            
+        </div><!-- fim da div geral --> 
+    </body>
+</html>
